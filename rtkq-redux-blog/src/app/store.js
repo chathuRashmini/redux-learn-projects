273fc20bba6f5632bd9c086d416a7e05fcf52ab1
chathu_08 <chathu.rash97@gmail.com>
2022-06-02 10:56:37 +0530

    Completed the project

--- rtkq-redux-blog/src/app/store.js
+++ rtkq-redux-blog/src/app/store.js
@@ -1,11 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postsReducer from '../features/posts/postsSlice';
-import usersReducer from '../features/users/usersSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
+import usersReducer from '../features/users/usersSlice';
+import { apiSlice } from "./apiSlice";
 
 export const store = configureStore({
     reducer: {
-        posts: postsReducer,
-        users: usersReducer
-    }
+        users: usersReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+
+    middleware: getDefaultMiddleware => 
+        getDefaultMiddleware().concat(apiSlice.middleware)
 })
